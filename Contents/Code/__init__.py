8ff87799c18788a12ced7456c8f76d71951a5591
sander1 <sanderspies+plexapp@gmail.com>
2011-04-14 20:41:34 +0200

    Catch empty containers

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,37 +3,29 @@ import re
 
 ###################################################################################################
 
-PLUGIN_TITLE               = 'Studio 100'
-PLUGIN_PREFIX              = '/video/studio100'
-BASE_URL                   = 'http://www.studio100tv.be'
-OVERVIEW_RSS               = '%s/rss' % BASE_URL
-VIDEO_FILE                 = '%s/videofile/%%s' % BASE_URL
-PLAYER                     = 'http://www.plexapp.com/player/player.php?clip=%s'
+PLUGIN_TITLE = 'Studio 100'
+BASE_URL     = 'http://www.studio100tv.be'
+OVERVIEW_RSS = '%s/rss' % BASE_URL
+VIDEO_FILE   = '%s/videofile/%%s' % BASE_URL
+PLAYER       = 'http://www.plexapp.com/player/player.php?clip=%s'
 
-# Art and icons
-ART_DEFAULT                = 'art-default.png'
-ICON_DEFAULT               = 'icon-default.png'
+ART_DEFAULT  = 'art-default.jpg'
+ICON_DEFAULT = 'icon-default.png'
 
 ###################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, ICON_DEFAULT, ART_DEFAULT)
+  Plugin.AddPrefixHandler('/video/studio100', MainMenu, PLUGIN_TITLE, ICON_DEFAULT, ART_DEFAULT)
+  Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
 
-  Plugin.AddViewGroup('Category', viewMode='List', mediaType='items')
-  Plugin.AddViewGroup('Details', viewMode='InfoList', mediaType='items')
-
-  # Set the default MediaContainer attributes
   MediaContainer.title1    = PLUGIN_TITLE
-  MediaContainer.viewGroup = 'Category'
+  MediaContainer.viewGroup = 'List'
   MediaContainer.userAgent = ''
   MediaContainer.art       = R(ART_DEFAULT)
 
-  # Set the default cache time
   HTTP.CacheTime = CACHE_1DAY
-  HTTP.Headers['User-agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.4) Gecko/20100611 Firefox/3.6.4'
+  HTTP.Headers['User-agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16'
 
 ###################################################################################################
-
 def MainMenu():
   dir = MediaContainer()
 
@@ -42,10 +34,12 @@ def MainMenu():
     url = f.xpath('./a')[0].get('href')
     dir.Append(Function(DirectoryItem(TVShow, title=title, thumb=R(ICON_DEFAULT)), url=url))
 
-  return dir
+  if len(dir) == 0:
+    return MessageContainer('Geen programma\'s', 'Er werden geen programma\'s gevonden')
+  else:
+    return dir
 
 ####################################################################################################
-
 def TVShow(sender, url):
   dir = MediaContainer(title2=sender.itemTitle)
 
@@ -54,15 +48,16 @@ def TVShow(sender, url):
     link = e.xpath('./link')[0].text.split('?', 1)[0].rsplit('/', 1)[1]
     video = VIDEO_FILE % link
     thumb = e.xpath('./enclosure')[0].text
-#   thumb = re.sub('([0-9]{1,})x([0-9]{1,})$', '640x360', thumb, 1)
     pubDate = e.xpath('./pubDate')[0].text
     date = Datetime.ParseDate(pubDate).strftime('%d/%m/%Y')
     dir.Append(WebVideoItem(PLAYER % String.Quote(video, usePlus=False), title=title, thumb=Function(GetThumb, url=thumb), infoLabel=date))
 
-  return dir
+  if len(dir) == 0:
+    return MessageContainer('Geen afleveringen', 'Er werden geen afleveringen gevonden')
+  else:
+    return dir
 
 ####################################################################################################
-
 def GetThumb(url):
   try:
     data = HTTP.Request(url, cacheTime=CACHE_1MONTH).content
